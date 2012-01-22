0152facbd8f98996cf923fcae411e72e97302ace
Robert Harm <robert@harm.co.at>
2012-01-22 23:45:14 +0100

    added directions to popuptext for markers on layer maps

--- leaflet-geojson.php
+++ leaflet-geojson.php
@@ -21,6 +21,7 @@ echo 'The WordPress plugin <a href="http://www.mapsmarker.com" target="_blank">L
 global $wpdb;
 $table_name_markers = $wpdb->prefix.'leafletmapsmarker_markers';
 $table_name_layers = $wpdb->prefix.'leafletmapsmarker_layers';
+$lmm_options = get_option( 'leafletmapsmarker_options' );
 if (isset($_GET['layer'])) {
   $layer = mysql_real_escape_string($_GET['layer']); //info: not intval() cause otherwise $layer=0 when creating new layer and showing all markers with layer id = 0
   
@@ -73,7 +74,21 @@ if (isset($_GET['layer'])) {
 	echo '"lat":"'.$marker['mlat'].'",'.PHP_EOL;
 	echo '"lon":"'.$marker['mlon'].'",'.PHP_EOL;
 	echo '"icon":"'.$micon_url.'",'.PHP_EOL;
-	echo '"text":"' . stripslashes(str_replace('"', '\"', preg_replace('/(\015\012)|(\015)|(\012)/','<br/>',$marker['mpopuptext']))) . '",'.PHP_EOL;
+
+	$mpopuptext = stripslashes(str_replace('"', '\"', preg_replace('/(\015\012)|(\015)|(\012)/','<br/>',$marker['mpopuptext'])));
+	if ( ($lmm_options['directions_popuptext_panel'] == 'yes') && ($lmm_options['directions_provider'] == 'googlemaps') ) { 
+	$avoidhighways = (isset($lmm_options[ 'directions_googlemaps_route_type_highways' ] ) == TRUE ) && ( $lmm_options[ 'directions_googlemaps_route_type_highways' ] == 1 ) ? '&dirflg=h' : '';
+	$avoidtolls = (isset($lmm_options[ 'directions_googlemaps_route_type_tolls' ] ) == TRUE ) && ( $lmm_options[ 'directions_googlemaps_route_type_tolls' ] == 1 ) ? '&dirflg=t' : '';
+	$publictransport = (isset($lmm_options[ 'directions_googlemaps_route_type_public_transport' ] ) == TRUE ) && ( $lmm_options[ 'directions_googlemaps_route_type_public_transport' ] == 1 ) ? '&dirflg=r' : '';
+	$walking = (isset($lmm_options[ 'directions_googlemaps_route_type_walking' ] ) == TRUE ) && ( $lmm_options[ 'directions_googlemaps_route_type_walking' ] == 1 ) ? '&dirflg=w' : '';
+	$mpopuptext = $mpopuptext . "<div style='border-top:1px solid #f0f0e7;padding-top:5px;margin-top:5px;'><a href='http://maps.google.com/maps?daddr=" . $marker['mlat'] . "," . $marker['mlon'] . "&t=" . $lmm_options[ 'directions_googlemaps_map_type' ] . "&layer=" . $lmm_options[ 'directions_googlemaps_traffic' ] . "&doflg=" . $lmm_options[ 'directions_googlemaps_distance_units' ] . $avoidhighways . $avoidtolls . $publictransport . $walking . "&hl=" . $lmm_options[ 'directions_googlemaps_host_language' ] . "&om=" . $lmm_options[ 'directions_googlemaps_overview_map' ] . "' target='_blank' title='" . esc_attr__('Get directions','lmm') . "'>" . __('Directions','lmm') . "</a></div>"; 
+	} else if ( ($lmm_options['directions_popuptext_panel'] == 'yes') && ($lmm_options['directions_provider'] == 'yours') ) {
+	$mpopuptext = $mpopuptext . "<div style='border-top:1px solid #f0f0e7;padding-top:5px;margin-top:5px;'><a href='http://www.yournavigation.org/?tlat=" . $marker['mlat'] . "&tlon=" . $marker['mlon'] . "&v=" . $lmm_options[ 'directions_yours_type_of_transport' ] . "&fast=" . $lmm_options[ 'directions_yours_route_type' ] . "&layer=" . $lmm_options[ 'directions_yours_layer' ] . "' target='_blank' title='" . esc_attr__('Get directions','lmm') . "'>" . __('Directions','lmm') . "</a></div>"; 
+	} else if ( ($lmm_options['directions_popuptext_panel'] == 'yes') && ($lmm_options['directions_provider'] == 'ors') ) {
+	$mpopuptext = $mpopuptext . "<div style='border-top:1px solid #f0f0e7;padding-top:5px;margin-top:5px;'><a href='http://openrouteservice.org/index.php?end=" . $marker['mlon'] . "," . $marker['mlat'] . "&pref=" . $lmm_options[ 'directions_ors_route_preferences' ] . "&lang=" . $lmm_options[ 'directions_ors_language' ] . "&noMotorways=" . $lmm_options[ 'directions_ors_no_motorways' ] . "&noTollways=" . $lmm_options[ 'directions_ors_no_tollways' ] . "' target='_blank' title='" . esc_attr__('Get directions','lmm') . "'>" . __('Directions','lmm') . "</a></div>"; 
+	}
+
+	echo '"text":"' . $mpopuptext . '",'.PHP_EOL;
 	echo '"zoom":"' . $marker['mzoom'] . '",'.PHP_EOL;
 	echo '"openpopup":"' . $marker['mopenpopup'] . '",'.PHP_EOL;
 	echo '"mapwidth":"' . $marker['mmapwidth'] . '",'.PHP_EOL;
