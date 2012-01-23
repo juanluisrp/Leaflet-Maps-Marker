f4329e8fa8ea58c2a4ea1a68f877f87ef213f084
Robert Harm <robert@harm.co.at>
2012-01-23 20:13:16 +0100

    changed plugin active-check

--- leaflet-georss.php
+++ leaflet-georss.php
@@ -2,19 +2,17 @@
 /*
     GeoRSS generator - Leaflet Maps Marker Plugin
 */
-//info: construct path to wp-load.php and get $wp_path
+//info: construct path to wp-load.php
 while(!is_file('wp-load.php')){
   if(is_dir('../')) chdir('../');
   else die('Error: Could not construct path to wp-load.php - please check <a href="http://mapsmarker.com/path-error">http://mapsmarker.com/path-error</a> for more details');
 }
 include( 'wp-load.php' );
-$wp_path_file = split('wp-content', __FILE__);
-$wp_path = $wp_path_file[0];
-//info: is plugin active?
-include_once( $wp_path.'wp-admin' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'plugin.php' );
 $format = (isset($_GET['format']) == TRUE ) ? $_GET['format'] : '';
 function hide_email($email) { $character_set = '+-.0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'; $key = str_shuffle($character_set); $cipher_text = ''; $id = 'e'.rand(1,999999999); for ($i=0;$i<strlen($email);$i+=1) $cipher_text.= $key[strpos($character_set,$email[$i])]; $script = 'var a="'.$key.'";var b=a.split("").sort().join("");var c="'.$cipher_text.'";var d="";'; $script.= 'for(var e=0;e<c.length;e++)d+=b.charAt(a.indexOf(c.charAt(e)));'; $script.= 'document.getElementById("'.$id.'").innerHTML="<a href=\\"mailto:"+d+"\\">"+d+"</a>"'; $script = "eval(\"".str_replace(array("\\",'"'),array("\\\\",'\"'), $script)."\")"; $script = '<script type="text/javascript">/*<![CDATA[*/'.$script.'/*]]>*/</script>'; return '<span id="'.$id.'">[javascript protected email address]</span>'.$script; }
-if (!is_plugin_active('leaflet-maps-marker/leaflet-maps-marker.php')) {
+//info: check if plugin is active (didnt use is_plugin_active() due to problems reported by users)
+function lmm_is_plugin_active( $plugin ) { return in_array( $plugin, (array) get_option( 'active_plugins', array() ) ); }
+if ( !lmm_is_plugin_active('leaflet-maps-marker/leaflet-maps-marker.php') ) {
 echo 'The WordPress plugin <a href="http://www.mapsmarker.com" target="_blank">Leaflet Maps Marker</a> is inactive on this site and therefore this API link is not working.<br/><br/>Please contact the site owner (' . hide_email(get_bloginfo('admin_email')) . ') who can activate this plugin again.';
 } else {
 global $wpdb;
